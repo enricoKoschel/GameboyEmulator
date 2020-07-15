1ada1184b89d673936e198f68dbb2b4103a664ed
enricoKoschel <enricokoschel02@gmail.com>
2020-07-15 04:10:38 +0200

    Graphics are almost working now, except for *THE WAVEEEEEE*!!! And some other minor changes.

--- GameboyEmulator/Graphics.cs
+++ GameboyEmulator/Graphics.cs
@@ -48,21 +48,26 @@ namespace GameboyEmulator
 				ushort tileDataIndex = lcd.TileDataBaseAddress;
 
 				if (lcd.TileDataIsSigned)
-					tileDataIndex += (byte)((sbyte)memory.Read(tileMapIndex) + 128);
+					tileDataIndex += (ushort)(((sbyte)memory.Read(tileMapIndex) + 128) * 16);
 				else
-					tileDataIndex += memory.Read(tileMapIndex);
+					tileDataIndex += (ushort)(memory.Read(tileMapIndex) * 16);
 
-				byte currentTileLine = (byte)(lcd.CurrentScanline % 8);
+				byte currentTileLine = (byte)((lcd.CurrentScanline % 8) * 2);
 				byte tileDataLo      = memory.Read((ushort)(tileDataIndex + currentTileLine));
 				byte tileDataHi      = memory.Read((ushort)(tileDataIndex + currentTileLine + 1));
 
 				for (int tilePixelIndex = 0; tilePixelIndex < 8; tilePixelIndex++)
 				{
-					int paletteIndexHi = Cpu.GetBit(tileDataHi, tilePixelIndex) ? 1 : 0;
-					int paletteIndexLo = Cpu.GetBit(tileDataLo, tilePixelIndex) ? 1 : 0;
+					int  paletteIndexLo = Cpu.GetBit(tileDataLo, tilePixelIndex) ? 1 : 0;
+					int  paletteIndexHi = Cpu.GetBit(tileDataHi, tilePixelIndex) ? 1 : 0;
 					byte paletteIndex   = (byte)((paletteIndexHi << 1) | paletteIndexLo);
 
-					screen.Buffer[(tileMapX - lcd.ScrollX) * 8 + tilePixelIndex, lcd.CurrentScanline].FillColor =
+					int tilePixelIndexReverse = tilePixelIndex - 7;
+					tilePixelIndexReverse *= -1;
+					int bufferXIndex = (tileMapX - lcd.ScrollX) * 8 + tilePixelIndexReverse;
+					int bufferYIndex = lcd.CurrentScanline;
+
+					screen.Buffer[bufferXIndex, bufferYIndex].FillColor =
 						GetColor(lcd.TilePalette, paletteIndex);
 				}
 			}
@@ -79,7 +84,8 @@ namespace GameboyEmulator
 			int colorIdLo = paletteIndex * 2;
 			int colorIdHi = colorIdLo + 1;
 
-			int colorId = (colorIdHi << 1) | colorIdLo;
+			int colorId = ((Cpu.GetBit(palette, colorIdHi) ? 1 : 0) << 1) |
+						  (Cpu.GetBit(palette, colorIdLo) ? 1 : 0);
 
 			return colorId switch
 			{
