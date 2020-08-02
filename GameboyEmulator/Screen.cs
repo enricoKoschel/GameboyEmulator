195c2f680bf05405a2f89850d3d2b6fb1e1e96d3
enricoKoschel <enricokoschel02@gmail.com>
2020-08-02 03:36:48 +0200

    Started experimenting with adding Color Modes.

--- GameboyEmulator/Screen.cs
+++ GameboyEmulator/Screen.cs
@@ -1,20 +1,48 @@
 ﻿using System;
+using System.Diagnostics;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
 
 namespace GameboyEmulator
 {
-	readonly struct Colors
-	{
-		public static Color black     = new Color(8, 24, 32);
-		public static Color darkGray  = new Color(52, 104, 86);
-		public static Color lightGray = new Color(136, 192, 112);
-		public static Color white     = new Color(224, 248, 208);
-	}
-
 	class Screen
 	{
+		public enum ColorModes
+		{
+			standardGameboy,
+			invertedGameboy,
+			blackAndWhite,
+			invertedBlackAndWhite
+		}
+		
+		public struct Colors
+		{
+			//Standard Gameboy
+			public static Color black     = new Color(8, 24, 32);
+			public static Color darkGray  = new Color(52, 104, 86);
+			public static Color lightGray = new Color(136, 192, 112);
+			public static Color white     = new Color(224, 248, 208);
+		
+			//Inverted Gameboy
+			//public static Color white     = new Color(8, 24, 32);
+			//public static Color lightGray  = new Color(52, 104, 86);
+			//public static Color darkGray = new Color(136, 192, 112);
+			//public static Color black     = new Color(224, 248, 208);
+		
+			//Inverted Black and White
+			//public static Color white = new Color(0, 0, 0);
+			//public static Color lightGray  = new Color(85, 85, 85);
+			//public static Color darkGray = new Color(170, 170, 170);
+			//public static Color black     = new Color(255, 255, 255);
+		
+			//Black and White
+			// public static Color black     = new Color(0, 0, 0);
+			// public static Color darkGray  = new Color(85, 85, 85);
+			// public static Color lightGray = new Color(170, 170, 170);
+			// public static Color white     = new Color(255, 255, 255);
+		}
+		
 		private const int SCREEN_GAME_WIDTH  = 160, SCREEN_GAME_HEIGHT = 144;
 		private const int SCREEN_SCALE       = 8;
 		private const int SCREEN_DRAW_WIDTH  = SCREEN_GAME_WIDTH * SCREEN_SCALE;
@@ -22,10 +50,15 @@ namespace GameboyEmulator
 
 		private readonly RenderWindow window;
 
+		private ColorModes colorMode;
+		public  Colors     colors;
+		
 		public RectangleShape[,] Buffer { get; }
 
-		public Screen()
+		public Screen(ColorModes colorMode)
 		{
+			this.colorMode = colorMode;
+			
 			Buffer = new RectangleShape[SCREEN_GAME_WIDTH, SCREEN_GAME_HEIGHT];
 			window = new RenderWindow(
 				new VideoMode(SCREEN_DRAW_WIDTH, SCREEN_DRAW_HEIGHT), "GameBoy Emulator", Styles.Close
